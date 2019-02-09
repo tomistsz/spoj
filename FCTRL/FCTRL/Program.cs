612d66bb2fb04b0472916e6c0c10b1de1577f99a
Tomasz Adamiec <tomaso1993@gmail.com>
2019-02-09 22:00:07 +0100

    FCTRL: Created project in VS

---
+++ FCTRL/FCTRL/Program.cs
@@ -0,0 +1,14 @@
+﻿// FCTRL - Factorial
+// https://www.spoj.com/problems/FCTRL/
+using System;
+
+namespace FCTRL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
