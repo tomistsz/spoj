85a3d398af2cf7a240a98b9cd729e27ed096647b
Tomasz Adamiec <tomaso1993@gmail.com>
2019-02-14 20:43:26 +0100

    FCTRL: Częściowe rozwiązanie opisane w komentarzu

--- FCTRL/FCTRL/Program.cs
+++ FCTRL/FCTRL/Program.cs
@@ -8,7 +8,28 @@ namespace FCTRL
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int t = Int32.Parse(Console.ReadLine());
+
+            for (int i = 0; i < t; i++)
+            {
+                int n = Int32.Parse(Console.ReadLine());
+                int zerosCount = 0;
+
+                for (int j = 1; j <= n; j++)
+                {
+                    int tempJ = j;
+                    while (tempJ % 5 == 0)
+                    {
+                        tempJ = tempJ / 5;
+                        zerosCount++;
+                    }
+
+                }
+
+                Console.WriteLine(zerosCount);
+                // odejmuj do liczby podzielnej przez 5
+                // dziel j / 5 tak długo > 0, % + % .... = wynik
+            }
         }
     }
 }
