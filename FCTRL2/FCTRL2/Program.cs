222151596915f19cedd27da21422a7e03b534c49
Tomasz Adamiec <tomaso1993@gmail.com>
2019-02-14 21:45:34 +0100

    FCTRL2: Created project in VS

---
+++ FCTRL2/FCTRL2/Program.cs
@@ -0,0 +1,14 @@
+﻿// FCTRL2 - Small factorials
+// https://www.spoj.com/problems/FCTRL2/
+using System;
+
+namespace FCTRL2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
