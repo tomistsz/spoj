b382aceab0b0a424a89873bc60c7ae078812e4c6
Tomasz Adamiec <tomaso1993@gmail.com>
2019-02-16 09:53:22 +0100

    FCTRL2: Problem Solved AC

--- FCTRL2/FCTRL2/Program.cs
+++ FCTRL2/FCTRL2/Program.cs
@@ -1,6 +1,7 @@
 ﻿// FCTRL2 - Small factorials
 // https://www.spoj.com/problems/FCTRL2/
 using System;
+using System.Collections.Generic;
 
 namespace FCTRL2
 {
@@ -8,7 +9,49 @@ namespace FCTRL2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int t = Int32.Parse(Console.ReadLine());
+            for (int i = 0; i < t; i++)
+            {
+                int maxElementValue = (int)Math.Pow(10,3);
+                List<int> resultList = new List<int>();
+
+                int n = Int32.Parse(Console.ReadLine());
+                resultList.Add(1);
+
+                for (int j = 1; j <= n; j++)
+                {
+                    int overflow = 0;
+                    for (int k = 0; k < resultList.Count; k++)
+                    {
+                        int partResult = resultList[k] * j + overflow;
+                        if (partResult < maxElementValue)
+                        {
+                            resultList[k] = partResult;
+                            overflow = 0;
+                        }
+                        else
+                        {
+                            resultList[k] = partResult % maxElementValue;
+                            overflow = partResult / maxElementValue;
+                        }
+                    }
+                    if (overflow > 0)
+                    {
+                        resultList.Add(overflow);
+                    }
+
+                }
+
+                for (int z = resultList.Count - 1; z >= 0; z--)
+                {
+                    if (z == resultList.Count - 1)
+                        Console.Write(resultList[z]);
+                    else
+                        Console.Write(String.Format("{0:000}", resultList[z]));
+                }
+                Console.WriteLine();
+
+            }
         }
     }
 }
