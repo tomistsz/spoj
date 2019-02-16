64c11e340d37df56a461b4959c05be84762342df
Tomasz Adamiec <tomaso1993@gmail.com>
2019-02-16 10:15:03 +0100

    ONP: Created project.

---
+++ ONP/ONP/Program.cs
@@ -0,0 +1,15 @@
+﻿// ONP - Transform the Expression
+// https://www.spoj.com/problems/ONP/
+
+using System;
+
+namespace ONP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ONP - Transform the Expression");
+        }
+    }
+}
