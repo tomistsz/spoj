188d0fb578924e1db94edac2759a3ac9b571e292
Tomasz Adamiec <tomaso1993@gmail.com>
2019-02-17 21:26:55 +0100

    NSTEPS: Created VS project

---
+++ NSTEPS/NSTEPS/Program.cs
@@ -0,0 +1,14 @@
+﻿// NSTEPS - Number Steps
+// https://www.spoj.com/problems/NSTEPS/
+using System;
+
+namespace NSTEPS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
